1397a5dbe56999a415c5a075851a49936e9a4499
JayJay The Second <stonesteve2711@gmail.com>
2022-05-18 07:59:20 +0300

    initial commit

---
+++ app/src/main/java/com/example/steveme/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.steveme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Get the nav host.
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        // Get the controller from nav host.
+        val navController = navHostFragment.navController
+        // Attach the van controller to bottom navigation.
+        findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+            .setupWithNavController(navController)
+    }
+}
